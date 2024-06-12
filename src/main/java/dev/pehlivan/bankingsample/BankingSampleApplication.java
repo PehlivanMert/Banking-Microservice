0b35525fb283e4c152213e3e6da2e6ee4a143025
PehlivanMert <pehlivannmert@gmail.com>
2024-06-12 17:04:06 +0300

    first commit

---
+++ src/main/java/dev/pehlivan/bankingsample/BankingSampleApplication.java
@@ -0,0 +1,13 @@
+package dev.pehlivan.bankingsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/pehlivan/bankingsample/BankingSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pehlivan.bankingsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
